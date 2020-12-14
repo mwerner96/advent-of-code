ff734e8630164e5f4c38a953af9b974fc38332ed
Maximilian Schier <23127402+m-schier@users.noreply.github.com>
2020-12-14 15:40:19 +0100

    Day 13: Better performance
    Use Chinese remainder theorem to improve runtime complexity of second part

--- d13/part2.py
+++ d13/part2.py
@@ -1,12 +1,29 @@
+from operator import mul
+from functools import reduce
+
+def gcd(a, b):
+    if b == 0:
+        return (a, 1, 0)
+    else:
+        d, si, ti = gcd(b, a % b)
+        return (d, ti, si - (a // b) * ti)
+
 f = open('input','r').readlines()
 
-schedule = [int(bus) if bus != "x" else 1 for bus in f[1].split(',')]
+schedule = [int(bus) if bus != "x" else None for bus in f[1].split(',')]
 
-time = schedule[0]
-factor = 1
-for idx, bus in enumerate(schedule[1:]):
-    while (time + idx + 1) % bus:
-        time += factor * schedule[0]
-    factor *= bus
+lcm = reduce(mul, (s for s in schedule if s is not None), 1)
+
+time = 0
+
+for offset, bus in enumerate(schedule):
+    if bus is None: continue
+    
+    M = lcm // bus
+    d, _, y = gcd(bus, M)
+
+    if d != 1: raise ValueError("Not coprime")
+
+    time = (time + ((bus - offset) % bus) * y * M) % lcm
 
 print(time)
