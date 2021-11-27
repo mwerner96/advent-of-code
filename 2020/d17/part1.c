cb6d0fd61767858151973ef66f3bb958def354a2
Maximilian Werner <maximilian.werner96@gmail.com>
2021-11-27 23:24:58 +0100

    2020 D17

---
+++ 2020/d17/part1.c
@@ -0,0 +1,61 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#define CYCLES 6
+#define INIT_SIZE 8
+#define EDGE ((CYCLES + 1) * 2 + INIT_SIZE)
+
+int main()
+{
+    bool dimension[EDGE][EDGE][EDGE] = {false};
+    bool next_dim[EDGE][EDGE][EDGE] = {false};
+    char c;
+    int x = CYCLES + 1;
+    int y = CYCLES + 1;
+    int z = CYCLES + 1;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+        {
+            x = CYCLES + 1;
+            y++;
+        }
+        else
+        {
+            dimension[x][y][z] = c == '#';
+            x++;
+        }
+    }
+    int cycle = CYCLES;
+    int count;
+    while (cycle--)
+    {
+        count = 0;
+        for (int x = 1; x < EDGE - 1; x++)
+            for (int y = 1; y < EDGE - 1; y++)
+                for (int z = 1; z < EDGE - 1; z++)
+                {
+                    int live = 0;
+                    bool active = dimension[x][y][z];
+                    for (int _x = -1; _x <= 1; _x++)
+                        for (int _y = -1; _y <= 1; _y++)
+                            for (int _z = -1; _z <= 1; _z++)
+                                live += dimension[_x + x][_y + y][_z + z] ? 1 : 0;
+                    if (active && ((live == 3) || (live == 4)))
+                        active = true;
+                    else if (!active && (live == 3))
+                        active = true;
+                    else
+                        active = false;
+                    next_dim[x][y][z] = active;
+                }
+        for (int x = 0; x < EDGE; x++)
+            for (int y = 0; y < EDGE; y++)
+                for (int z = 0; z < EDGE; z++)
+                {
+                    dimension[x][y][z] = next_dim[x][y][z];
+                    count += dimension[x][y][z] ? 1 : 0;
+                }
+    }
+    printf("%d\n", count);
+}
