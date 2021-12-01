4be158caaf555c0b36be1f2e3c1feeb0632e21c8
Maximilian Werner <maximilian.werner96@gmail.com>
2021-12-01 16:46:58 +0100

    2021 D01 optimize part 2

--- 2021/d01/part2.c
+++ 2021/d01/part2.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+#define OPTIMIZED
 #define WINDOW 3
 
 int next_idx[] = {1, 2, 3, 0};
 
+#ifndef OPTIMIZED
 int sum(int *buf, int idx)
 {
     int sum = buf[idx];
     sum += buf[next_idx[idx]];
     return sum + buf[next_idx[next_idx[idx]]];
 }
+#endif
 
 int main()
 {
@@ -19,7 +22,11 @@ int main()
     scanf("%d\n", &windows[idx = next_idx[idx]]);
     while (scanf("%d\n", &windows[idx = next_idx[idx]]) != EOF)
     {
+#ifndef OPTIMIZED
         increased += sum(windows, next_idx[idx]) < sum(windows, next_idx[next_idx[idx]]) ? 1 : 0;
+#else
+        increased += windows[next_idx[idx]] < windows[idx] ? 1 : 0;
+#endif
     }
     printf("%d\n", increased);
 }
