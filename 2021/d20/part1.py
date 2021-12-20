090c51c5f2c8dfbac7fab876cc3559d707ce2e94
Maximilian Werner <maximilian.werner96@gmail.com>
2021-12-20 15:44:31 +0100

    2021 D20

---
+++ 2021/d20/part1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy import ndimage
+
+iterations = 2
+
+with open('input') as f:
+    filter = [c == '#' for c in f.readline().strip()]
+    f.readline()
+    image = np.pad(np.genfromtxt(f.readlines(), dtype=str, delimiter=1, comments=None) == '#', iterations)
+
+for _ in range(iterations):
+    image = ndimage.generic_filter(image, lambda a: filter[int(''.join(['1' if b else '0' for b in a.flatten()]), 2)], size=3, mode='nearest')
+
+print(np.count_nonzero(image))
