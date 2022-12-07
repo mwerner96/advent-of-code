26b42c8aa40d22a487fe0e3caab87e21631a2542
Maximilian Werner <maximilian.werner96@gmail.com>
2022-12-07 16:16:35 +0100

    2022 D07

---
+++ 2022/d07/part2.py
@@ -0,0 +1,36 @@
+lines = open("input").read().splitlines()
+
+pwd = []
+tree = {}
+
+for l in lines:
+    if l.startswith("$"):
+        cmd = l.split()
+        if cmd[1] == "cd":
+            if cmd[2] == "..":
+                pwd.pop()
+            else:
+                pwd.append(cmd[2])
+    else:
+        first, second = l.split()
+        path = "/".join(pwd)
+        if path not in tree:
+            tree[path] = []
+        if first != "dir":
+            tree[path].append((int(first), second))
+
+dirsize = {}
+for dir, files in tree.items():
+    dirsize[dir] = sum(size for size, _ in files)
+
+recsize = {}
+for dir in tree.keys():
+    recsize[dir] = 0
+    for other in tree.keys():
+        if dir in other:
+            recsize[dir] += dirsize[other]
+
+for size in sorted(recsize.values()):
+    if (70000000 - recsize["/"]) + size >= 30000000:
+        print(size)
+        break
