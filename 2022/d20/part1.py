b2886980d075a9c8059944df917cc05173d3c6c3
Maximilian Werner <maximilian.werner@e-t-a.de>
2022-12-20 13:36:37 +0100

    2022 D20

---
+++ 2022/d20/part1.py
@@ -0,0 +1,18 @@
+original = [(idx, int(n)) for idx, n in enumerate(open("input").read().splitlines())]
+
+decrypted = [n for n in original]
+
+LEN = len(original)
+
+for idx, n in original:
+    old_idx = decrypted.index((idx, n))
+    new_idx = (old_idx + n) % (LEN - 1)
+    if (old_idx != new_idx) and (new_idx == 0):
+        new_idx = LEN - 1
+    decrypted.pop(old_idx)
+    decrypted.insert(new_idx, (idx, n))
+
+flat_decrypted = [n for _, n in decrypted]
+idx_zero = flat_decrypted.index(0)
+
+print(sum(flat_decrypted[(idx_zero + coord) % LEN] for coord in (1000, 2000, 3000)))
