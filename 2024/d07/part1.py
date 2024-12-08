b48151edc584aa1e2d8ce59bd8806403766175d3
Maximilian Werner <maximilian.werner96@gmail.com>
2024-12-08 16:10:46 +0100

    2024 D07

---
+++ 2024/d07/part1.py
@@ -0,0 +1,24 @@
+from itertools import product
+
+
+with open("input") as f:
+    eqns = []
+    for line in f.read().splitlines():
+        result, eqn = line.split(": ")
+        result = int(result)
+        eqn = [int(x) for x in eqn.split()]
+        eqns.append((result, eqn))
+
+sum = 0
+for result, eqn in eqns:
+    for operations in product(
+        [lambda a, b: a + b, lambda a, b: a * b], repeat=len(eqn) - 1
+    ):
+        calc = eqn[0]
+        for idx, (operation, val) in enumerate(tuple(zip(operations, eqn[1:]))):
+            calc = operation(calc, val)
+        if calc == result:
+            sum += result
+            break
+
+print(sum)
